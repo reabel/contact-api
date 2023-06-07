d79977a45d68427fef4036a55aa532e8d7ea8ff8
reabel <ryanreabel@gmail.com>
2023-06-07 10:28:20 -0400

    initial commits

---
+++ routes.js
@@ -0,0 +1,18 @@
+// Desc: Routes for the application
+// Export the routes
+module.exports = [
+    {
+        path: '/',
+        method: 'GET',
+        handler: (req, res) => {
+            res.end('Hello World\n');
+        }
+    },
+    {
+        path: '/users',
+        method: 'GET',
+        handler: (req, res) => {
+            res.end('User list');
+        }
+    }
+];
